f23f8198e48a990079c0b06f7a9f1a227f2f7f65
Yiğitcan Ölmez <yigitcanolmez@outlook.com>
2023-05-05 09:19:56 +0300

    refactoring

--- ETicaretAPI/Core/ETicaretAPI.Application/Repositories/IReadRepository.cs
+++ ETicaretAPI/Core/ETicaretAPI.Application/Repositories/IReadRepository.cs
@@ -3,15 +3,14 @@ using System.Linq.Expressions;
 
 namespace ETicaretAPI.Application.Repositories
 {
-    public interface IReadRepository<T> :IRepository<T>
+    public interface IReadRepository<T> : IRepository<T>
         where T : BaseEntity
     {
-        IQueryable<T> GetAll();
-        IQueryable<T> GetWhere(Expression<Func<T, bool>> method);
+        IQueryable<T> GetAll(bool tracking = true);
+        IQueryable<T> GetWhere(Expression<Func<T, bool>> method, bool tracking = true);
+        Task<T> GetSingleAsync(Expression<Func<T, bool>> method, bool tracking = true);
 
-        Task<T> GetSingleAsync(Expression<Func<T, bool>> method);
-
-        Task<T> GetByIdAsync(string id);
+        Task<T> GetByIdAsync(string id, bool tracking = true);
 
     }
-} 
+}
